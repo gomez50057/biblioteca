47b0fc9b9959ae8f12cc9b66d782ceeac306bd6f
Gabriel Gomez <gomez.50057@gmail.com>
2025-08-03 21:58:29 -0600

    actualizacion a nexts

--- src/components/CardContent.js
+++ src/components/CardContent.js
@@ -1,9 +1,12 @@
 import CardsGrid from "@/components/card/CardsGrid";
+import styles from '@/styles/CardContent.module.css';
 
 export default function CardContent() {
   return (
-    <main>
-      <CardsGrid />
-    </main>
+    <section className={styles.container}>
+      <div className={styles.screen}>
+        <CardsGrid />
+      </div>
+    </section>
   );
 }
